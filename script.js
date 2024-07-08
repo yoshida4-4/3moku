21b5542ea934bc9e2c2848c1cdbfdc0d11d008c4
yoshida4-4 <rosepearl_grey@yahoo.co.jp>
2024-07-08 16:08:24 +0900

    none

---
+++ script.js
@@ -0,0 +1,10 @@
+const field = document.querySelector('#field')
+const body = document.querySelector('body')
+console.log(body)
+
+for (let i = 1; i < 26; i++) {
+    field.insertAdjacentHTML('beforeend', `
+        <div class='areas' id=area${i}></div>
+    `)
+}
+
