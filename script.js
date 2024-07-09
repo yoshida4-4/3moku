e7ce50712bdc799570a9b483361e00ce20320f97
yoshida4-4 <rosepearl_grey@yahoo.co.jp>
2024-07-09 13:11:30 +0900

    0709start

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const field = document.querySelector('#field')
-const body = document.querySelector('body')
-console.log(body)
+// const body = document.querySelector('body')
+// console.log(body)
 
 for (let i = 1; i < 26; i++) {
     field.insertAdjacentHTML('beforeend', `
         <div class='areas' id=area${i}></div>
     `)
+    document.querySelector(`#area${i}`).addEventListener('click', () => {
+        
+    })
 }
 
